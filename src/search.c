a57089e3cb897dbc2e78e7166942c81a801db3db
Mayke Wallace <53350761+Nitrox0Af@users.noreply.github.com>
2022-03-10 13:34:46 -0300

    search consertado

--- src/search.c
+++ src/search.c
@@ -8,8 +8,27 @@
 #include "search.h"
 
 void search(Suffix **suf, int context, int lgt, char *key){
-    int lgt_key = strlen(key);
+    String* aux=create_string(key);
+    for(int i=0; i<lgt; i++){
+        int lgt;
+        if(aux->len < suf[i]->s->len - suf[i]->index) lgt = aux->len;
+        else lgt = suf[i]->s->len - suf[i]->index;
+        int a=suf[i]->index;
+        int verificador=0;
+        for(int j=0; j<lgt; j++){
+            if (toupper(suf[i]->s->c[j + a]) != toupper(aux->c[j])) {
+                verificador=1;
+                break;
+            }
+        }
+        if(verificador==0){
+            if(a>context && context+aux->len+suf[i]->index <= suf[i]->s->len)
+                print_substring(suf[i]->s, a - context, a + context + aux->len);
+        }
 
+    }
+    destroy_string(aux);
+    /*int lgt_key = strlen(key);
     for (int i = 0; i < lgt && lgt_key > 0; i++){
         int a = (suf[i]->index);
         if (*(suf[i]->s->c + a) > *key)
@@ -20,8 +39,7 @@ void search(Suffix **suf, int context, int lgt, char *key){
             {
                 print_substring(suf[i]->s, a - context, a + context + lgt_key);
             }
-        }
-    }
+        }*/
 
     /*int lgt_key = strlen(key);
 
@@ -39,8 +57,5 @@ void search(Suffix **suf, int context, int lgt, char *key){
                 }
         }
     }*/
-
-
-
 
 }
